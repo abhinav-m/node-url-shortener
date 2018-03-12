9ab68229ea75f08102b5f515afabafd60b8c6a5d
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-12 16:42:34 +0530

    Added scaffolding for route GET/:id and GET/new/:url

--- server/server.js
+++ server/server.js
@@ -1,13 +1,41 @@
 const path = require('path');
+const fs = require('fs');
 
+// const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-// Middleware to serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+// Middleware to use json in server.
+// app.use(bodyParser.json());
 
-app.get('/api/:id', (req, res) => {});
+// Simple middleware to create log file.
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/:id', (req, res) => {
+  // TODO: Fetch shortened url from db.
+  const id = req.params.id;
+  res.status(200).send(id);
+});
+
+app.get('/new/:url', (req, res) => {
+  // TODO: Check if url is correct, if it is create record in db and send back response.
+  // If not, send back error json response.
+  const url = req.params.url;
+  res.status(200).send(url);
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
